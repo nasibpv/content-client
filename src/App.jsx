c9c1684f836285cbfa03464c416964c81c641c65
nasibpv <nasibnpv@gmail.com>
2024-08-28 23:03:58 +0530

    connect with server

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import Index from './Components/Index'
+import Home from './Components/Home'
 
 function App() {
 
   return (
     <>
-      <Index/>
+      <Router>
+      <Routes>
+          <Route path='/' element={<Index/>}></Route>
+          <Route path='/home' element={<Home/>}></Route>
+      </Routes>
+        </Router>
+     
     </>
   )
 }
--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Index() {
+    const [email,setEmail]=useState('')
+    const[pwd,setPwd]=useState('')
+    const navigate=useNavigate()
+    const submit=async(e)=>{
+        const body={
+            userId:email,
+            password:pwd
+          }
+          console.log(body);
+          const { data } = await axios.post('http://localhost:8000/loginForm',body)
+          console.log(data);
+          if(data.status){
+            alert(data.message)
+          alert('Welcome Luminar technoLab')
+          alert('okk')
+                //     localStorage.setItem("usertype", JSON.stringify(data.usertype))
+                // localStorage.setItem("name", JSON.stringify(data.username))
+                // localStorage.setItem("mail", JSON.stringify(data.email))
+                // localStorage.setItem("token", JSON.stringify(data.token))
+      
+        //   location('/')
+          }
+          else{
+            alert(data.message)
+      
+          }
+       
+        // navigate('/home')
+    }
   return (
     <div>
         <div className='w-full flex justify-center aline-center items-center content-center p-5 h-[100vh]'>
             <div className='flex flex-col gap-5 p-5 border '>
-                <input type="text" placeholder='email' className='border p-2'/>
-                <input type="password" placeholder='password' className='border p-2' />
-                <button className='button p-2 bg-green-500 rounded border ' >Submit</button>
+                <input type="text" placeholder='email' className='border p-2' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                <input type="password" placeholder='password' className='border p-2'value={pwd} onChange={(e)=>setPwd(e.target.value)} />
+                <button className='button p-2 bg-green-500 rounded border ' type='submit' onClick={submit}>Submit</button>
             </div>
+         
         </div>
     </div>
   )
